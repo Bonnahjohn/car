4081c59322355eae7e17a9a724cbae9f671e81bb
bonnah joohn <bonnahpoku@gmail.com>
2023-02-08 10:37:16 +0000

    This website is purposely to advertise to other about where they can get their car repairing

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** 
+ * 
+//  * @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
+  plugins: [require("tw-elements/dist/plugin")],
+};
